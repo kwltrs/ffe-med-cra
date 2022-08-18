6167bf7ec4ae7a5e2b2a9bbcfc402cd6979623c5
Kristofer Walters <kristofer.walters@sparebank1.no>
2022-08-18 10:27:51 +0200

    Include less from @sb1/ffe

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+import { Heading1 } from '@sb1/ffe-core-react';
+import { ActionButton } from '@sb1/ffe-buttons-react';
+import logo from '@sb1/ffe-sb1-logos/svg/logo-sb1.svg';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Heading1>SpareBank 1 Designsystem</Heading1>
+        <ActionButton element="a" href="https://design.sparebank1.no">Besøk vår hjemmeside</ActionButton>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
